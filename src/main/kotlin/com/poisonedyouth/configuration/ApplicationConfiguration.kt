60ea47181fa702e1a2ec79f32da2ef8fd554a34a
Matthias Schenk <matthias.schenk@sevdesk.de>
2022-10-14 22:05:29 +0200

    [V-16] Added security settings to application configuration

--- src/main/kotlin/com/poisonedyouth/configuration/ApplicationConfiguration.kt
+++ src/main/kotlin/com/poisonedyouth/configuration/ApplicationConfiguration.kt
@@ -4,6 +4,7 @@ import io.ktor.server.application.Application
 
 class ApplicationConfiguration {
     lateinit var databaseConfig: DatabaseConfig
+    lateinit var securityConfig: SecurityConfig
 }
 
 fun Application.setupApplicationConfiguration(): ApplicationConfiguration {
@@ -16,7 +17,30 @@ fun Application.setupApplicationConfiguration(): ApplicationConfiguration {
     val user = databaseObject.property("user").getString()
     val password = databaseObject.property("password").getString()
     val maxPoolSize = databaseObject.property("maxPoolSize").getString().toInt()
-    appConfig.databaseConfig = DatabaseConfig(driverClass, url, user, password, maxPoolSize)
+    appConfig.databaseConfig = DatabaseConfig(
+        driverClass = driverClass,
+        url = url,
+        user = user,
+        password = password,
+        maxPoolSize = maxPoolSize
+    )
+
+    // Security
+    val securityObject = environment.config.config("ktor.security")
+    val fileIntegrityCheckHashingAlgorithm = securityObject.property("fileIntegrityCheckHashingAlgorithm").getString()
+    val defaultPasswordKeySize = securityObject.property("defaultPasswordKeySize").getString().toInt()
+    val defaultNonceLength = securityObject.property("defaultNonceLength").getString().toInt()
+    val defaultSaltLength = securityObject.property("defaultSaltLength").getString().toInt()
+    val defaultIterationCount = securityObject.property("defaultIterationCount").getString().toInt()
+    val defaultGcmParameterSpecLength = securityObject.property("defaultGcmParameterSpecLength").getString().toInt()
+    appConfig.securityConfig = SecurityConfig(
+        fileIntegrityCheckHashingAlgorithm = fileIntegrityCheckHashingAlgorithm,
+        defaultPasswordKeySize = defaultPasswordKeySize,
+        defaultNonceLength = defaultNonceLength,
+        defaultSaltLength = defaultSaltLength,
+        defaultIterationCount = defaultIterationCount,
+        defaultGcmParameterSpecLength = defaultGcmParameterSpecLength
+    )
     return appConfig
 }
 
@@ -26,4 +50,13 @@ data class DatabaseConfig(
     val user: String,
     val password: String,
     val maxPoolSize: Int
+)
+
+data class SecurityConfig(
+    val fileIntegrityCheckHashingAlgorithm: String,
+    val defaultPasswordKeySize: Int,
+    val defaultNonceLength: Int,
+    val defaultSaltLength: Int,
+    val defaultIterationCount: Int,
+    val defaultGcmParameterSpecLength: Int
 )
