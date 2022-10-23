24b0d68a9e866b974a3610d26358d8ef3ccaf594
Matthias Schenk <matthias.schenk@sevdesk.de>
2022-10-23 12:44:40 +0200

    [V-20] Added api tests (part 1)

--- src/test/kotlin/com/poisonedyouth/KtorServerExtension.kt
+++ src/test/kotlin/com/poisonedyouth/KtorServerExtension.kt
@@ -2,6 +2,12 @@ package com.poisonedyouth
 
 import com.poisonedyouth.application.deleteDirectoryRecursively
 import com.poisonedyouth.configuration.ApplicationConfiguration
+import io.ktor.client.HttpClient
+import io.ktor.client.plugins.auth.Auth
+import io.ktor.client.plugins.auth.providers.BasicAuthCredentials
+import io.ktor.client.plugins.auth.providers.basic
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.jackson.jackson
 import io.ktor.server.config.ApplicationConfig
 import io.ktor.server.engine.applicationEngineEnvironment
 import io.ktor.server.engine.connector
@@ -18,7 +24,6 @@ import org.junit.jupiter.api.extension.BeforeEachCallback
 import org.junit.jupiter.api.extension.ExtensionContext
 import java.nio.file.Files
 import java.nio.file.Path
-import java.nio.file.Paths
 
 class KtorServerExtension : BeforeAllCallback, BeforeEachCallback, AfterEachCallback, AfterAllCallback {
     companion object {
@@ -57,4 +62,21 @@ class KtorServerExtension : BeforeAllCallback, BeforeEachCallback, AfterEachCall
     override fun afterAll(context: ExtensionContext?) {
         server.stop(100, 100)
     }
+}
+
+fun createHttpClient(username: String = "username", password: String = "password"): HttpClient {
+    val client = HttpClient {
+        install(ContentNegotiation) {
+            jackson()
+        }
+        install(Auth) {
+            basic {
+                credentials {
+                    BasicAuthCredentials(username = username, password = password)
+                }
+                realm = "ktor-encryption-server"
+            }
+        }
+    }
+    return client
 }
---
+++ src/test/kotlin/com/poisonedyouth/application/ApiTest.kt
@@ -0,0 +1,203 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
+
+package com.poisonedyouth.application
+
+import com.poisonedyouth.KtorServerExtension
+import com.poisonedyouth.api.UpdatePasswordDto
+import com.poisonedyouth.api.UserDto
+import com.poisonedyouth.api.UserSettingsDto
+import com.poisonedyouth.configuration.ApplicationConfiguration
+import com.poisonedyouth.createHttpClient
+import com.poisonedyouth.domain.SecuritySettings
+import com.poisonedyouth.domain.User
+import com.poisonedyouth.domain.UserSettings
+import com.poisonedyouth.persistence.UploadFileEntity
+import com.poisonedyouth.persistence.UploadFileHistoryEntity
+import com.poisonedyouth.persistence.UserEntity
+import com.poisonedyouth.persistence.UserRepository
+import com.poisonedyouth.security.EncryptionManager
+import io.ktor.client.call.body
+import io.ktor.client.request.delete
+import io.ktor.client.request.forms.formData
+import io.ktor.client.request.forms.submitFormWithBinaryData
+import io.ktor.client.request.post
+import io.ktor.client.request.put
+import io.ktor.client.request.setBody
+import io.ktor.http.ContentType
+import io.ktor.http.Headers
+import io.ktor.http.HttpHeaders
+import io.ktor.http.HttpStatusCode
+import io.ktor.http.contentType
+import kotlin.io.path.listDirectoryEntries
+import kotlin.io.path.readBytes
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.assertj.core.api.Assertions.*
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.koin.test.KoinTest
+import org.koin.test.inject
+import java.nio.file.Files
+import java.util.*
+
+@ExtendWith(KtorServerExtension::class)
+class ApiTest : KoinTest {
+    private val userRepository by inject<UserRepository>()
+    private val userService by inject<UserService>()
+
+    @BeforeEach
+    fun clearDatabase() {
+        transaction {
+            UploadFileHistoryEntity.all().forEach { it.delete() }
+            UploadFileEntity.all().forEach { it.delete() }
+            UserEntity.all().forEach { it.delete() }
+        }
+    }
+
+    @Test
+    fun `create new user is possible`() = runTest {
+        // given
+        val client = createHttpClient()
+
+        // when
+        val response = client.post("http://localhost:8080/api/user") {
+            setBody(
+                UserDto(
+                    username = "poisonedyouth",
+                    password = "!1Wertzueu482835",
+                    userSettings = UserSettingsDto(
+                        uploadFileExpirationDays = 12
+                    )
+                )
+            )
+            contentType(ContentType.Application.Json)
+        }
+
+        // then
+        assertThat(response.status).isEqualTo(HttpStatusCode.Created)
+        val result = response.body<String>()
+        assertThat(result).isNotNull
+        assertThat(transaction { UserEntity.getUserOrThrow("poisonedyouth") }).isNotNull
+    }
+
+    @Test
+    fun `delete user is possible`() = runTest {
+        // given
+        val user = persistUser("poisonedyouth", "Ab1!999999999999")
+
+        val client = createHttpClient(username = user.username, password = "Ab1!999999999999")
+
+        // when
+        val response = client.delete("http://localhost:8080/api/user") {
+            contentType(ContentType.Application.Json)
+        }
+
+        // then
+        assertThat(response.status).isEqualTo(HttpStatusCode.OK)
+        val result = response.body<String>()
+        assertThat(result).isNotNull
+        assertThat(transaction { UserEntity.findUserOrNull("poisonedyouth") }).isNull()
+    }
+
+    @Test
+    fun `update user password is possible`() = runTest {
+        // given
+        val user = persistUser("poisonedyouth", "Ab1!999999999999")
+
+        val client = createHttpClient(username = user.username, password = "Ab1!999999999999")
+
+        // when
+        val response = client.put("http://localhost:8080/api/user/password") {
+            setBody(
+                UpdatePasswordDto(
+                    "123456789aB!rtzu"
+                )
+            )
+            contentType(ContentType.Application.Json)
+        }
+
+        // then
+        assertThat(response.status).isEqualTo(HttpStatusCode.OK)
+        val result = response.body<String>()
+        assertThat(result).isNotNull
+        assertThatNoException().isThrownBy {
+            userService.authenticate(
+                UserDto(
+                    username = user.username,
+                    password = "123456789aB!rtzu",
+                )
+            )
+        }
+    }
+
+    @Test
+    fun `update user settings is possible`() = runTest {
+        // given
+        val user = persistUser("poisonedyouth", "Ab1!999999999999")
+
+        val client = createHttpClient(username = user.username, password = "Ab1!999999999999")
+
+        // when
+        val response = client.put("http://localhost:8080/api/user/settings") {
+            setBody(
+                UserSettingsDto(
+                    51
+                )
+            )
+            contentType(ContentType.Application.Json)
+        }
+
+        // then
+        assertThat(response.status).isEqualTo(HttpStatusCode.OK)
+        val result = response.body<String>()
+        assertThat(result).isNotNull
+        assertThat(transaction
+                   {
+                       UserEntity.findUserOrNull("poisonedyouth")!!.userSettings.uploadFileExpirationDays
+                   }).isEqualTo(51)
+    }
+
+    @Test
+    fun `upload file is working`() = runTest {
+        // given
+        val user = persistUser("poisonedyouth", "Ab1!999999999999")
+
+        val client = createHttpClient(username = user.username, password = "Ab1!999999999999")
+
+        // when
+        val uploadFile = Files.createTempFile("file1.txt", "")
+        Files.writeString(uploadFile, "Hello World!")
+        val response = client.submitFormWithBinaryData(
+            url = "http://localhost:8080/api/upload",
+            formData = formData {
+                append("file", uploadFile.readBytes(), Headers.build {
+                    append(HttpHeaders.ContentType, "text/plain")
+                    append(HttpHeaders.ContentDisposition, "filename=file1.txt")
+                })
+            }
+        )
+
+        // then
+        assertThat(response.status).isEqualTo(HttpStatusCode.Created)
+        assertThat(ApplicationConfiguration.getUploadDirectory().listDirectoryEntries()).hasSize(1)
+    }
+
+    private fun persistUser(username: String, password: String): User {
+        val user = User(
+            username = username,
+            encryptionResult = EncryptionManager.encryptPassword(password),
+            userSettings = UserSettings(uploadFileExpirationDays = 50),
+            securitySettings = SecuritySettings(
+                fileIntegrityCheckHashingAlgorithm = "SHA-512",
+                passwordKeySizeBytes = 256,
+                nonceLengthBytes = 32,
+                saltLengthBytes = 64,
+                iterationCount = 10000,
+                gcmParameterSpecLength = 128
+            )
+        )
+        return userRepository.save(user)
+    }
+}
