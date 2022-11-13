b81265715a52c221c9babc0e1b8af1fc81b182b6
Matthias Schenk <matthias.schenk@sevdesk.de>
2022-11-13 19:34:27 +0100

    [V-30] Extracted command to own file

--- client/src/main/kotlin/Application.kt
+++ client/src/main/kotlin/Application.kt
@@ -1,36 +1,11 @@
-import io.ktor.client.HttpClient
-import io.ktor.client.engine.cio.CIO
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.serialization.jackson.jackson
 import kotlinx.cli.ArgParser
-import kotlinx.cli.ArgType
 import kotlinx.cli.ExperimentalCli
-import kotlinx.cli.Subcommand
-import kotlinx.cli.required
-import kotlinx.coroutines.runBlocking
+import operations.NewUserCommand
 
 @OptIn(ExperimentalCli::class)
-suspend fun main(args: Array<String>) {
-    val client = createHttpClient()
+fun main(args: Array<String>) {
     val parser = ArgParser("ktor-encryption-server")
 
-    class NewUser : Subcommand("newUser", "Create New User") {
-        val username by parser.option(ArgType.String, shortName = "n", description = "New User").required()
-        val password by parser.option(ArgType.String, shortName = "p", description = "User Password").required()
-
-        override fun execute() {
-            runBlocking {
-                createNewUser(client, username, password)
-            }
-        }
-    }
-
-    parser.subcommands(NewUser())
+    parser.subcommands(NewUserCommand(parser))
     parser.parse(args)
-}
-
-private fun createHttpClient() = HttpClient(CIO) {
-    install(ContentNegotiation) {
-        jackson()
-    }
 }
--- client/src/main/kotlin/Operations.kt
+++ client/src/main/kotlin/Operations.kt
@@ -1,17 +1,20 @@
 import com.poisonedyouth.api.UserDto
 import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.client.statement.bodyAsText
 import io.ktor.http.ContentType
 import io.ktor.http.HttpStatusCode
-import io.ktor.http.HttpStatusCode.Companion
 import io.ktor.http.contentType
+import io.ktor.serialization.jackson.jackson
 
 const val BASE_URL = "http://0.0.0.0:8080/api"
 
+val client = createHttpClient()
 suspend fun createNewUser(client: HttpClient, username: String, userPassword: String) {
-    val result =  client.post("$BASE_URL/user") {
+    val result = client.post("$BASE_URL/user") {
         setBody(
             UserDto(
                 username = username,
@@ -20,9 +23,15 @@ suspend fun createNewUser(client: HttpClient, username: String, userPassword: St
         )
         contentType(ContentType.Application.Json)
     }
-    if(result.status == HttpStatusCode.Created) {
+    if (result.status == HttpStatusCode.Created) {
         println(result.bodyAsText())
-    } else{
+    } else {
         println("Creation of new user failed because of '${result.bodyAsText()}' (${result.status})")
+    }
+}
+
+private fun createHttpClient() = HttpClient(CIO) {
+    install(ContentNegotiation) {
+        jackson()
     }
 }
